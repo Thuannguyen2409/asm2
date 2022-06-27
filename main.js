a3c32c8e86e160d64814344264b66c4bb8eec3f1
Thuannguyen2409 <76115620+Thuannguyen2409@users.noreply.github.com>
2022-06-27 08:52:12 +0700

    make it better

--- main.js
+++ main.js
@@ -72,6 +72,6 @@ app.get('/insert', (req, res)=>{
 //     res.render('allProduct')
 // })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 6000
 app.listen(PORT)
 console.log('Runing....')
